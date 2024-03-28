c5a89da6ee557e54a0529f0e02e2215cc5b06ede
comba, yannick <y.comba@maine-et-loire.fr>
2024-03-28 15:14:31 +0100

    clean code

--- ajax/changeSettingsVisibility.php
+++ ajax/changeSettingsVisibility.php
@@ -24,7 +24,7 @@
  You should have received a copy of the GNU General Public License
  along with MyCustomView. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
- */
+*/
 
 include("../../../inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
